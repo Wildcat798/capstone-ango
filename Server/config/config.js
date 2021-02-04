e97b88d8424e61a6cfc0b118416b33c540ec136b
Anthony Meleah <anthony.meleah@gmail.com>
2021-02-04 16:04:25 -0500

    signup and login stuff

--- Server/config/config.js
+++ Server/config/config.js
@@ -5,19 +5,20 @@ module.exports = {
     database: process.env.DB_NAME,
         host: process.env.DB_HOST,
     dialect: 'postgres',
-  }
-//   "test": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_test",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   },
-//   "production": {
-//     "username": "root",
-//     "password": null,
-//     "database": "database_production",
-//     "host": "127.0.0.1",
-//     "dialect": "mysql"
-//   }
+  },
+  // "test": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_test",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // },
+  // "production": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "database_production",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql"
+  // }
 }
+
